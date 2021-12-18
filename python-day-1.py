225117ef059910e46dee6e475fd2b4b4681260ad
Adil khan <adilkhan1117@gmail.com>
2021-12-18 21:20:24 +0000

    Added Python Day 1

---
+++ python-day-1.py
@@ -0,0 +1,41 @@
+# Day 1:
+
+**Variable example** 
+
+name = "Tom"
+
+city = "London"
+
+**Input example**
+
+input("my name is ? ") 
+
+**Print**
+
+print(name) = Tom
+
+print("name") = name (this will print name as you are using approciphe
+
+**len**
+
+print(len("adil")) = 4 (as 4 characters) 
+
+**Adding objects together**
+
+print("name " + "city ") = tom London
+
+**adding print and len together** 
+
+print(len(input("what is your name ? ")))
+
+**printing inside a print statement and also variable:**
+
+print("hello" + "  " +  'hi ')  = hello hi
+
+**print with input**
+
+print("hello " + input("how are you ? ")) = how are you ? ..... / hello ....
+
+**next space below**
+
+print("how are you today \n")
