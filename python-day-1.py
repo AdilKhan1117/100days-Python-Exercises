ab62a1b34f76ece1359d7de448482c81d3087a85
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2021-12-18 21:24:08 +0000

    updated day 1 python layout

--- python-day-1.py
+++ python-day-1.py
@@ -3,38 +3,45 @@
 **Variable example** 
 
 name = "Tom"
-
 city = "London"
+
 
 **Input example**
 
 input("my name is ? ") 
 
+
 **Print**
 
 print(name) = Tom
 
-print("name") = name (this will print name as you are using approciphe
+print("name") = name #(this will print name as you are using quotes)
+                      
 
 **len**
 
-print(len("adil")) = 4 (as 4 characters) 
+print(len("adil")) = 4 (as in add 4 characters starting with binary of 0) 
+
 
 **Adding objects together**
 
 print("name " + "city ") = tom London
 
+
 **adding print and len together** 
 
 print(len(input("what is your name ? ")))
+
 
 **printing inside a print statement and also variable:**
 
 print("hello" + "  " +  'hi ')  = hello hi
 
+
 **print with input**
 
 print("hello " + input("how are you ? ")) = how are you ? ..... / hello ....
+
 
 **next space below**
 
