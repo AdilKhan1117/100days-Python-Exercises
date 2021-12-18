4fe91a55306ef384e5465f94e38a1487e79b45e4
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2021-12-18 21:32:30 +0000

    Update python-day2.py

--- python-day2.py
+++ python-day2.py
@@ -4,9 +4,9 @@ print("hello"[0]) = H
 
 print(123 + 345) = 4568
 
-whereas, string not required
+**whereas, string not required**
 
-print("123) + ("345") = 123345
+print("123") + ("345") = 123345
 
 **Interger**
 
@@ -21,16 +21,14 @@ print(1.5 + 1.5) = 3.0
 **Boolean: No quotation mark**
 
 True 
-
 False
 
 **Len does not work with integers** 
 
-if your not sure ever what type of data type this is 
+**if your not sure ever what type of data type this is 
+you can always do this**
 
-you can always do this
-
-**print(type(num_char))**
+print(type(num_char))
 
 num_char = len(input("how old are you ? "))
 
@@ -41,7 +39,6 @@ print("what is your name" " " + new_numb )
 To comment out lines just do this:
 
 **command button on mac**
-
 **then this backslash /**
 
 
