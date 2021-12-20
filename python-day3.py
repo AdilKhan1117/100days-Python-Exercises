6f4cbe3036f43dfc2be1ce9e388331c5cb49570e
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2021-12-20 17:38:44 +0000

    Update python-day3.py
    updating text to help others identify common scenerious

--- python-day3.py
+++ python-day3.py
@@ -2,8 +2,12 @@ Day 3 Orojects:
 
 print("Welcome to the coding club")
 
-path1 = input('Hello\'s peeps, welcome to the coding club, where do you want to go ? "golang" or "python" \n').lower()
+#If you have a comma for example, intentionally i had put a comman stating 'hello's', just so i could show, you cannot have all double quotes as golang and python are inputs -
+#This needs to be in this format
 
+path1 = input('Hello\'s peeps, welcome to the coding club, where do you want to go ? "golang" or "python" \n').lower() #lower was used so if anyone typed in caps it would user lower case instead
+
+# If you can see the lines are indented according to the action the condition is taking place.
 if path1 == "python":
   path2 = input("do you want to purchase a course, no or yes ? ")
   if path2 == "yes":
@@ -20,3 +24,4 @@ if path1 == "python":
     print("see you soon")
 else:
   ("wrong choice")
+#the above else block is accoring to the first line of code, line 11, else block.
