1c87202c7729e3e876e52aea9dc220dee05a4ad8
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-01-03 16:26:02 +0000

    Update python-day5.py

--- python-day5-code-challenges/python-day5.py
+++ python-day5-code-challenges/python-day5.py
@@ -1,1 +1,13 @@
+Using for Loops. These are used to loops blocks of cpde (repeat)
 
+fruits = ["Apple", "Peach", "Pears"]
+
+for fruits in fruits:
+  print(fruits) #This will loop around and print each object inside fruits 
+  print(fruits + "pie") #This will loop and add 'pie' to each object, like this 
+  
+  #apple pie
+  #peach pie
+  #pears pie
+  
+  
