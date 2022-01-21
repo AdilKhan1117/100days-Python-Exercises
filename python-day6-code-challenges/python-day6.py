448c5ca4a0bd479e670a20267a59787b66b8bdf7
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-01-21 23:40:47 +0000

    Update python-day6.py

--- python-day6-code-challenges/python-day6.py
+++ python-day6-code-challenges/python-day6.py
@@ -1,1 +1,21 @@
+Creating Python Functions:
+Goal was to move the robot to the flag using function and using for loop to loop 6 times. 
+  
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+ 
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
 
+
+for char in range(6):
+    jump()
