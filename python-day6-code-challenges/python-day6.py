ee8c8636a0a27ef05846dd46cf185682298db62f
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-01-24 21:41:19 +0000

    Update python-day6.py

--- python-day6-code-challenges/python-day6.py
+++ python-day6-code-challenges/python-day6.py
@@ -1,3 +1,5 @@
+Challenge 1:
+  
 Creating Python Functions:
 Goal was to move the robot to the flag using function and using for loop to loop 6 times. 
   
@@ -31,8 +33,32 @@ while number_of_hurdles > 0:
   print(number_of_hurdles)
     
     
-    
-    
+Challenge 2: This challenge states if the robot hits the objective in a while loop it stops whenever it has completed its end goes. 
+  
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+ 
+number_of_hurdles = 6 
+
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    if at_goal():
+        done()
+    print(number_of_hurdles)
     
     
     
