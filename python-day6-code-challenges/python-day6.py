a246f07cb70e004801f29d2ae1ea9757e0760de8
ADIL KHAN <64632642+AdilKhan1117@users.noreply.github.com>
2022-02-02 22:58:24 +0000

    Update python-day6.py

--- python-day6-code-challenges/python-day6.py
+++ python-day6-code-challenges/python-day6.py
@@ -92,6 +92,30 @@ while not at_goal():
     else:
         move()
     
+Challenge 4:
+  
+def move_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    move_right()
+    move()
+    move_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+        
     
     
     
